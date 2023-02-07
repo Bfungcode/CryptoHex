110e3fbf966950972ab18d18a519cb6f91aff216
BfungCode <105415086+rleBcode@users.noreply.github.com>
2023-02-07 12:30:36 +0700

    DONE

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -290,19 +290,15 @@ export default {
             apiData: []
         }
     },
-    mounted() {
-      this.cryptoPrice();
-      setInterval(() => {
-        this.cryptoPrice()
-      }, 5000);
-      console.log(this.apiData)
-    },
     methods: {
       async cryptoPrice() {
         const response = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false');
         this.apiData = response.data
       },
     },
+    created() {
+      this.cryptoPrice()
+    }
     }
 </script>
 
